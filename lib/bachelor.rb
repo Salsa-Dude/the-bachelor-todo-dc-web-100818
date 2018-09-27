728c410b70fed366a1319bfa2339699ce44f7f7a
Robin Tram <robin.tram@gmail.com>
2018-09-27 15:38:16 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -63,6 +63,6 @@ def get_average_age_for_season(data, season)
   array.each do |i|
     array_sum = array_sum + i
   end 
-  array_avg (array_sum / array.length).ceil
+  array_avg = (array_sum / array.length).ceil
   binding.pry
 end
