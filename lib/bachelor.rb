04e3c914d3ae9f521efba77d9184beda507aa85d
Robin Tram <robin.tram@gmail.com>
2018-09-27 15:21:47 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,7 +52,8 @@ def get_average_age_for_season(data, season)
   data.each do |season_number, season_data|
     if season_number == season 
       season_data.each do |season_data|
-                binding.pry
+        array << season_data.values[2]
+        binding.pry
       end 
     end 
   end 
